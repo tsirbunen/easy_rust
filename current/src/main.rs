e66638c35decc9b8bc69bf5e6731251ec48838aa
Satu Hyvärinen <tsirbunen@gmailcom>
2023-03-26 11:15:36 +0300

    Uninitialized variables

--- current/src/main.rs
+++ current/src/main.rs
@@ -1,48 +1,20 @@
-fn print_number(number: u8) {
-    println!("Number: {number}");
-}
-
-fn print_string(string: String) {
-    println!("String: {string}");
-}
-
-fn string_length(string: &String) {
-    println!("Length: {}", string.split_whitespace().count())
-}
-
 fn main() {
-    let integer = 3;
-    // Why does this work? Earlier with String variable it did not!
-    // This is because the primitive type integer implements the Copy trait
-    // meaning that below (instead of giving ownership of the actual variable to the
-    // function we are only COPYING the value of the variable and
-    // passing that on)
-    // So the integer variable that we give away here is a NEW variable
-    // and we still hold the ownership to the original variable
-    print_number(integer);
-    // Therefore we can do this again and again (make a new copy)
-    print_number(integer);
-    print_number(integer);
+    // This is not allowed (rust does not know the type):
+    // let uninitialized_variable_1;
 
-    // But the above would / did not work for String because String does not
-    // implement the Copy trait. Instead it implements the .clone() method.
-    let string = String::from("India");
-    // Instead of the earlier, here we force rust to make a copy of the variable
-    // because it does not do that by itself (to copy Strings be default might be costly
-    // because the variable value of String could be like 100 pages of a book!).
-    // So here we give the function a copy (a CLONE) of the variable's value.
-    print_string(string.clone());
-    // And therefore we can call the function again and again
-    // (by making a new copy every time)
-    print_string(string.clone());
-    print_string(string.clone());
+    // This compiles, but we still get a warning about a possibly
+    // uninitialized variable:
+    let _uninitialized_variable_2: u8;
 
-    // However, the clone method for string should be used only when we really need it:
-    let mut growing_string = String::new();
-    for _ in 0..100 {
-        growing_string.push_str("word ");
-        // Note: Instead of creating here a clone (copy) of the string each time,
-        // we give a reference to the same string every time
-        string_length(&growing_string);
+    // This works, because we have BOTH the if and the else branches
+    // so it is guaranteed that the variable will be initialized
+    let uninitialized_variable_3: u8;
+    if true {
+        uninitialized_variable_3 = 5;
+    } else {
+        // If we did not have this else branch, rust would not compile
+        // complaining that the variable might be uninitialized
+        uninitialized_variable_3 = 6;
     }
+    println!("Value {}", uninitialized_variable_3);
 }
