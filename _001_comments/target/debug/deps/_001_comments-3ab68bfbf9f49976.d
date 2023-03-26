0e1e21df960ef939b9b2b1dc657fd8c610b4a039
Satu Hyvärinen <tsirbunen@gmailcom>
2023-03-26 11:57:14 +0300

    Restructure files

--- _001_comments/target/debug/deps/_001_comments-3ab68bfbf9f49976.d
+++
@@ -1,5 +0,0 @@
-/Users/satu/HOBBY/RUST/easy_rust/_001_comments/target/debug/deps/_001_comments-3ab68bfbf9f49976.rmeta: src/main.rs
-
-/Users/satu/HOBBY/RUST/easy_rust/_001_comments/target/debug/deps/_001_comments-3ab68bfbf9f49976.d: src/main.rs
-
-src/main.rs:
